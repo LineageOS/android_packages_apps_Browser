bf5e496dc1f5f69b6c24a03828e89849a5587235
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-11-19 00:34:48 +0000

    Revert "Runtime enablement of WebGL"
    This reverts commit 0059fc7aaf5a8c141f7753b2574240e7dee53885.
    
    Change-Id: I799965f86443f75a1e1576c54ffec31532c58ebe

--- src/com/android/browser/BrowserSettings.java
+++ src/com/android/browser/BrowserSettings.java
@@ -130,8 +130,6 @@ public class BrowserSettings implements OnSharedPreferenceChangeListener,
 
     private static String sFactoryResetUrl;
 
-    private static boolean sWebGLAvailable;
-
     public static void initialize(final Context context) {
         sInstance = new BrowserSettings(context);
     }
@@ -278,8 +276,6 @@ public class BrowserSettings implements OnSharedPreferenceChangeListener,
         settings.setSaveFormData(saveFormdata());
         settings.setUseWideViewPort(isWideViewport());
         settings.setAutoFillProfile(getAutoFillProfile());
-        setIsWebGLAvailable(settings.isWebGLAvailable());
-        settings.setWebGLEnabled(isWebGLAvailable() && isWebGLEnabled());
 
         String ua = mCustomUserAgents.get(settings);
         if (ua != null) {
@@ -846,10 +842,6 @@ public class BrowserSettings implements OnSharedPreferenceChangeListener,
         return 1 + (mPrefs.getInt(PREF_INVERTED_CONTRAST, 0) / 10f);
     }
 
-    public boolean isWebGLEnabled() {
-        return mPrefs.getBoolean(PREF_ENABLE_WEBGL, true);
-    }
-
     // -----------------------------
     // getter/setters for privacy_security_preferences.xml
     // -----------------------------
@@ -940,14 +932,6 @@ public class BrowserSettings implements OnSharedPreferenceChangeListener,
 
     public String getVideoPreloadEnabled() {
         return mPrefs.getString(PREF_VIDEO_PRELOAD, getDefaultVideoPreloadSetting());
-    }
-
-    private static void setIsWebGLAvailable(boolean available) {
-        sWebGLAvailable = available;
-    }
-
-    public static boolean isWebGLAvailable() {
-        return sWebGLAvailable;
     }
 
     // -----------------------------
