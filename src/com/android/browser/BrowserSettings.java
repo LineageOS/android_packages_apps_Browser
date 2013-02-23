fc6be1960d08359fb412fe5e900c74a94eda8689
Chirayu Desai <cdesai@cyanogenmod.org>
2013-02-23 19:13:56 +0530

    Allow enabling WebSockets from Labs Preferences
    Change-Id: Ia0dc06cbd4f2e8e340a8f8ee910a6310a5079d79
    
    labs: websockets: fix typo in preference key
    
    Change-Id: I7b32b9a8148e56103b683ed60f8314075d59e5a2

--- src/com/android/browser/BrowserSettings.java
+++ src/com/android/browser/BrowserSettings.java
@@ -265,6 +265,7 @@ public class BrowserSettings implements OnSharedPreferenceChangeListener,
         settings.setSavePassword(rememberPasswords());
         settings.setSaveFormData(saveFormdata());
         settings.setUseWideViewPort(isWideViewport());
+        settings.setWebSocketsEnabled(isWebSocketsEnabled());
 
         String ua = mCustomUserAgents.get(settings);
         if (ua != null) {
@@ -779,6 +780,10 @@ public class BrowserSettings implements OnSharedPreferenceChangeListener,
 
     public float getInvertedContrast() {
         return 1 + (mPrefs.getInt(PREF_INVERTED_CONTRAST, 0) / 10f);
+    }
+
+    public boolean isWebSocketsEnabled() {
+        return mPrefs.getBoolean(PREF_ENABLE_WEBSOCKETS, false);
     }
 
     // -----------------------------
