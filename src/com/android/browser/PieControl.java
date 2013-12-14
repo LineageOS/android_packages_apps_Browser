LineageOS/android_packages_apps_Browser

commit 5623a091605bf1d88687dc589a61ee6b54c554fe

Browser: disable incognito mode
Chromium Webview does not support incognito mode which will result
in force close, disabling for now.

Change-Id: I501a609f94c115b9a9d1755ef0ea73eb9b29c823

src/com/android/browser/PieControl.java
@@ -61,7 +61,6 @@ public class PieControl implements PieMenu.PieController, OnClickListener {
     private PieItem mHistory;
     private PieItem mAddBookmark;
     private PieItem mNewTab;
-    private PieItem mIncognito;
     private PieItem mClose;
     private PieItem mShowTabs;
     private PieItem mInfo;
@@ -139,7 +138,6 @@ public class PieControl implements PieMenu.PieController, OnClickListener {
         mRefresh = makeItem(R.drawable.ic_refresh_holo_dark, 1);
         mForward = makeItem(R.drawable.ic_forward_holo_dark, 1);
         mNewTab = makeItem(R.drawable.ic_new_window_holo_dark, 1);
-        mIncognito = makeItem(R.drawable.ic_new_incognito_holo_dark, 1);
         mClose = makeItem(R.drawable.ic_close_window_holo_dark, 1);
         mInfo = makeItem(android.R.drawable.ic_menu_info_details, 1);
         mFind = makeItem(R.drawable.ic_search_holo_dark, 1);
@@ -160,7 +158,7 @@ public class PieControl implements PieMenu.PieController, OnClickListener {
         stack.setAdapter(mTabAdapter);
         mShowTabs.setPieView(stack);
         setClickListener(this, mBack, mRefresh, mForward, mUrl, mFind, mInfo,
-                mShare, mBookmarks, mNewTab, mIncognito, mClose, mHistory,
+                mShare, mBookmarks, mNewTab, mClose, mHistory,
                 mAddBookmark, mOptions, mRDS);
         if (!BrowserActivity.isTablet(mActivity)) {
             mShowTabs.getView().setOnClickListener(this);
@@ -183,7 +181,6 @@ public class PieControl implements PieMenu.PieController, OnClickListener {
         mUrl.addItem(makeFiller());
         mPie.addItem(mShowTabs);
         mShowTabs.addItem(mClose);
-        mShowTabs.addItem(mIncognito);
         mShowTabs.addItem(mNewTab);
         mShowTabs.addItem(makeFiller());
         mPie.addItem(mBookmarks);
@@ -217,9 +214,6 @@ public class PieControl implements PieMenu.PieController, OnClickListener {
             mUiController.bookmarkCurrentPage();
         } else if (mNewTab.getView() == v) {
             mUiController.openTabToHomePage();
-            mUi.editUrl(false, true);
-        } else if (mIncognito.getView() == v) {
-            mUiController.openIncognitoTab();
             mUi.editUrl(false, true);
         } else if (mClose.getView() == v) {
             mUiController.closeCurrentTab();

res/layout-land/nav_screen.xml
@@ -41,14 +41,6 @@
             android:contentDescription="@string/accessibility_button_homescreen"
             android:src="@drawable/ic_home_holo_dark" />
         <ImageButton
-            android:id="@+id/newincognitotab"
-            android:layout_width="wrap_content"
-            android:layout_height="match_parent"
-            style="@style/HoloButton"
-            android:gravity="center_vertical"
-            android:contentDescription="@string/accessibility_button_newincognitotab"
-            android:src="@drawable/ic_new_incognito_holo_dark" />
-        <ImageButton
             android:id="@+id/newtab"
             android:layout_width="wrap_content"
             android:layout_height="match_parent"

res/layout/nav_screen.xml
@@ -42,14 +42,6 @@
             android:contentDescription="@string/accessibility_button_homescreen"
             android:src="@drawable/ic_home_holo_dark" />
         <ImageButton
-            android:id="@+id/newincognitotab"
-            android:layout_width="wrap_content"
-            android:layout_height="match_parent"
-            style="@style/HoloButton"
-            android:gravity="center_vertical"
-            android:contentDescription="@string/accessibility_button_newincognitotab"
-            android:src="@drawable/ic_new_incognito_holo_dark" />
-        <ImageButton
             android:id="@+id/newtab"
             android:layout_width="wrap_content"
             android:layout_height="match_parent"

res/menu/browser.xml
@@ -35,10 +35,6 @@
             android:showAsAction="never"
             android:alphabeticShortcut="n" />
         <item
-            android:id="@+id/incognito_menu_id"
-            android:title="@string/new_incognito_tab"
-            android:icon="@drawable/ic_new_incognito_holo_dark" />
-        <item
             android:id="@+id/bookmarks_menu_id"
             android:title="@string/bookmarks"
             android:icon="@drawable/ic_bookmarks_holo_dark"

res/menu/browsercontext.xml
@@ -40,8 +40,6 @@
             android:title="@string/contextmenu_openlink"/>
         <item android:id="@+id/open_newtab_context_menu_id"
             android:title="@string/contextmenu_openlink_newwindow"/>
-        <item android:id="@+id/open_newtab_incognito_context_menu_id"
-            android:title="@string/contextmenu_openlink_incognito_newwindow"/>
         <item android:id="@+id/save_link_context_menu_id"
             android:title="@string/contextmenu_savelink"/>
         <item android:id="@+id/copy_link_context_menu_id"

src/com/android/browser/Controller.java
@@ -1385,42 +1385,6 @@ public class Controller
                                 });
                     }
                 }
-                newTabItem = menu.findItem(R.id.open_newtab_incognito_context_menu_id);
-                newTabItem.setTitle(getSettings().openInBackground()
-                        ? R.string.contextmenu_openlink_incognito_newwindow_background
-                                : R.string.contextmenu_openlink_incognito_newwindow);
-                newTabItem.setVisible(showNewTab);
-                newTabItem.setVisible(!mTabControl.getCurrentTab().isPrivateBrowsingEnabled());
-                if (showNewTab) {
-                    if (WebView.HitTestResult.SRC_IMAGE_ANCHOR_TYPE == type) {
-                        newTabItem.setOnMenuItemClickListener(
-                                new MenuItem.OnMenuItemClickListener() {
-                                    @Override
-                                    public boolean onMenuItemClick(MenuItem item) {
-                                        final HashMap<String, WebView> hrefMap =
-                                            new HashMap<String, WebView>();
-                                        hrefMap.put("webview", webview);
-                                        final Message msg = mHandler.obtainMessage(
-                                                FOCUS_NODE_HREF,
-                                                R.id.open_newtab_context_menu_id,
-                                                1, hrefMap);
-                                        webview.requestFocusNodeHref(msg);
-                                        return true;
-                                    }
-                                });
-                    } else {
-                        newTabItem.setOnMenuItemClickListener(
-                                new MenuItem.OnMenuItemClickListener() {
-                                    @Override
-                                    public boolean onMenuItemClick(MenuItem item) {
-                                        final Tab parent = mTabControl.getCurrentTab();
-                                        openTab(extra, parent != null,
-                                                !mSettings.openInBackground(), true, parent);
-                                        return true;
-                                    }
-                                });
-                    }
-                }
                 if (type == WebView.HitTestResult.SRC_ANCHOR_TYPE) {
                     break;
                 }
@@ -1593,10 +1557,6 @@ public class Controller
             // -- Main menu
             case R.id.new_tab_menu_id:
                 openTabToHomePage();
-                break;
-
-            case R.id.incognito_menu_id:
-                openIncognitoTab();
                 break;
 
             case R.id.close_other_tabs_id:

src/com/android/browser/NavScreen.java
@@ -60,7 +60,6 @@ public class NavScreen extends RelativeLayout
     ImageButton mBookmarks;
     ImageButton mMore;
     ImageButton mHomeTab;
-    ImageButton mNewIncognitoTab;
     ImageButton mNewTab;
     FrameLayout mHolder;
 
@@ -128,12 +127,10 @@ public class NavScreen extends RelativeLayout
                 R.string.accessibility_transition_navscreen));
         mBookmarks = (ImageButton) findViewById(R.id.bookmarks);
         mHomeTab = (ImageButton) findViewById(R.id.gotohome);
-        mNewIncognitoTab = (ImageButton) findViewById(R.id.newincognitotab);
         mNewTab = (ImageButton) findViewById(R.id.newtab);
         mMore = (ImageButton) findViewById(R.id.more);
         mBookmarks.setOnClickListener(this);
         mHomeTab.setOnClickListener(this);
-        mNewIncognitoTab.setOnClickListener(this);
         mNewTab.setOnClickListener(this);
         mMore.setOnClickListener(this);
         mScroller = (NavTabScroller) findViewById(R.id.scroller);
@@ -161,8 +158,8 @@ public class NavScreen extends RelativeLayout
     public void onClick(View v) {
         if (mBookmarks == v) {
             mUiController.bookmarksOrHistoryPicker(ComboViews.Bookmarks);
-        } else if (mNewIncognitoTab == v || mNewTab == v) {
-            openNewTab(mNewIncognitoTab == v);
+        } else if (mNewTab == v) {
+            openNewTab(false);
         } else if (mHomeTab == v) {
             gotoHomePage();
         } else if (mMore == v) {

src/com/android/browser/PhoneUi.java
@@ -187,10 +187,6 @@ public class PhoneUi extends BaseUi {
         if (newtab != null && !mUseQuickControls) {
             newtab.setVisible(false);
         }
-        MenuItem incognito = menu.findItem(R.id.incognito_menu_id);
-        if (incognito != null) {
-            incognito.setVisible(showingNavScreen() || mUseQuickControls);
-        }
         MenuItem fullscreen = menu.findItem(R.id.fullscreen_menu_id);
         if (fullscreen != null) {
             fullscreen.setVisible(!showingNavScreen());

commit 06929f1b1103f5a5651a8fc71e6b6e6b10bde71d

Browser: fix pie crash
Introduced in 5623a091605bf1d88687dc589a61ee6b54c554fe.

Change-Id: I83f44be84fb70e2777ab1414c0af2a368c435fb8

src/com/android/browser/PieControl.java
@@ -180,6 +180,7 @@ public class PieControl implements PieMenu.PieController, OnClickListener {
         mUrl.addItem(makeFiller());
         mUrl.addItem(makeFiller());
         mPie.addItem(mShowTabs);
+        mShowTabs.addItem(makeFiller());
         mShowTabs.addItem(mClose);
         mShowTabs.addItem(mNewTab);
         mShowTabs.addItem(makeFiller());