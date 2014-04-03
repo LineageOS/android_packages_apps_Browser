3174b657e860a879035de4fd7a9cf3c1c5a6f785
Jorge Ruesga <jorge@ruesga.com>
2014-04-03 05:07:12 +0200

    browser: allow to load browser internal assets
    The new webview classes avoid to load urls from local file schemes. Just replace the load of internal browser assets (most_visited) from loadUrl to loadData.
    
    Change-Id: Iee633a1a3599a7e752e15452fa04e15baac2e256
    JIRA: CYAN-3564
    Issue: https://jira.cyanogenmod.org/browse/CYAN-3564
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/com/android/browser/Tab.java
+++ src/com/android/browser/Tab.java
@@ -68,11 +68,14 @@ import android.widget.Toast;
 
 import com.android.browser.TabControl.OnThumbnailUpdatedListener;
 import com.android.browser.homepages.HomeProvider;
+import com.android.browser.preferences.GeneralPreferencesFragment;
 import com.android.browser.provider.SnapshotProvider.Snapshots;
+import com.google.common.io.ByteStreams;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.util.LinkedList;
@@ -155,6 +158,8 @@ class Tab implements PictureListener {
     // before onPageFinsihed)
     private boolean mInPageLoad;
     private boolean mDisableOverrideUrlLoading;
+    // If true, the current page is the most visited page
+    private boolean mInMostVisitedPage;
     // The last reported progress of the current page
     private int mPageLoadProgress;
     // The time the load started, used to find load page time
@@ -388,6 +393,16 @@ class Tab implements PictureListener {
             }
             syncCurrentState(view, url);
             mWebViewController.onPageFinished(Tab.this);
+
+            if (view.getUrl().equals(HomeProvider.MOST_VISITED_URL)) {
+                if (!mInMostVisitedPage) {
+                    loadUrl(HomeProvider.MOST_VISITED, null);
+                    mInMostVisitedPage = true;
+                }
+                view.clearHistory();
+            } else {
+                mInMostVisitedPage = false;
+            }
         }
 
         // return true if want to hijack the url to let another app to handle it
@@ -579,14 +594,6 @@ class Tab implements PictureListener {
                 final HttpAuthHandler handler, final String host,
                 final String realm) {
             mWebViewController.onReceivedHttpAuthRequest(Tab.this, view, handler, host, realm);
-        }
-
-        @Override
-        public WebResourceResponse shouldInterceptRequest(WebView view,
-                String url) {
-            WebResourceResponse res = HomeProvider.shouldInterceptRequest(
-                    mContext, url);
-            return res;
         }
 
         @Override
@@ -1764,7 +1771,20 @@ class Tab implements PictureListener {
             mInPageLoad = true;
             mCurrentState = new PageState(mContext, false, url, null);
             mWebViewController.onPageStarted(this, mMainView, null);
-            mMainView.loadUrl(url, headers);
+            WebResourceResponse res = HomeProvider.shouldInterceptRequest(mContext, url);
+            if (res != null) {
+                try {
+                    String data = readWebResource(res).toString();
+                    mInMostVisitedPage = true;
+                    mMainView.loadDataWithBaseURL(url, data, res.getMimeType(), res.getEncoding(),
+                            HomeProvider.MOST_VISITED_URL);
+                } catch (IOException io) {
+                    // Fallback to default load handling
+                    mMainView.loadUrl(url, headers);
+                }
+            } else {
+                mMainView.loadUrl(url, headers);
+            }
         }
     }
 
@@ -1910,5 +1930,20 @@ class Tab implements PictureListener {
             // sub-resource.
             setSecurityState(SecurityState.SECURITY_STATE_MIXED);
         }
+    }
+
+    private StringBuilder readWebResource(WebResourceResponse response) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        InputStream is = response.getData();
+        try {
+            byte[] data = new byte[512];
+            int read = 0;
+            while ((read = is.read(data, 0, 512)) != -1) {
+                sb.append(new String(data, 0, read));
+            }
+        } finally {
+            is.close();
+        }
+        return sb;
     }
 }
