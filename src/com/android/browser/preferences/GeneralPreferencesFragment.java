4a972c48ab612491ea8004617fcaf9903019fe17
Tarun Nainani <tnainani@codeaurora.org>
2015-04-09 16:45:39 -0700

    Restore homepage dialog state on screen rotation.
    Homepage dialog activity was not saving search url text while
    doing screen rotation. Fixed issue via saving url text inside
    bundle when 'onSavedInstance' is called and restoring url
    from bundle when onCreateDialog is called.
    
    CR-Fixed: 724867
    
    Change-Id: I85e5ddce48f95c398959a2e64141c87e4c83e022

--- src/com/android/browser/preferences/GeneralPreferencesFragment.java
+++ src/com/android/browser/preferences/GeneralPreferencesFragment.java
@@ -224,6 +224,8 @@ public class GeneralPreferencesFragment extends PreferenceFragment
       Add this class to manage AlertDialog lifecycle.
     */
     public static class MyAlertDialogFragment extends DialogFragment {
+        private  final String HOME_PAGE = "homepage";
+        private EditText editText = null;
         public static MyAlertDialogFragment newInstance() {
             MyAlertDialogFragment frag = new MyAlertDialogFragment();
             return frag;
@@ -232,10 +234,12 @@ public class GeneralPreferencesFragment extends PreferenceFragment
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             final BrowserSettings settings = BrowserSettings.getInstance();
-            final EditText editText = new EditText(getActivity());
+            editText = new EditText(getActivity());
+            String homePage = savedInstanceState != null ?
+                    savedInstanceState.getString(HOME_PAGE): settings.getHomePage();
             editText.setInputType(InputType.TYPE_CLASS_TEXT
                     | InputType.TYPE_TEXT_VARIATION_URI);
-            editText.setText(settings.getHomePage());
+            editText.setText(homePage);
             editText.setSelectAllOnFocus(true);
             editText.setSingleLine(true);
             editText.setImeActionLabel(null, EditorInfo.IME_ACTION_DONE);
@@ -283,6 +287,12 @@ public class GeneralPreferencesFragment extends PreferenceFragment
                     WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
 
             return dialog;
+        }
+
+        @Override
+        public void onSaveInstanceState(Bundle outState){
+            super.onSaveInstanceState(outState);
+            outState.putString(HOME_PAGE, editText.getText().toString().trim());
         }
     }
 }
