9d321eae0ed3c7b47c06deec69476d919986b041
Brian Jolly <bkjolly77@gmail.com>
2015-04-22 14:06:02 -0400

    Make Browser useable in Leanback Launcher
    Change-Id: I0838b89f5186b125ded732520c853f75d85e182c

--- Android.mk
+++ Android.mk
@@ -7,7 +7,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
         android-common \
         guava \
         android-support-v13 \
-        android-support-v4 \
+        android-support-v4
 
 LOCAL_SRC_FILES := \
         $(call all-java-files-under, src) \
