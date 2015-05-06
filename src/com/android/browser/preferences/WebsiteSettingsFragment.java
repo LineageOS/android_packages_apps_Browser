ad9f693093f1d045ad9315a8e558d5bba989e763
linuxxxxx <joey@cyanogenmoditalia.it>
2015-05-06 20:59:59 +0200

    [WIP] Browser: update material theme
    Some updates to material theme:
    
    - Longpress fab to open incognito tab
    - Vectors
    - Rename resources
    - Cards like tabs
    
    ToDo:
    - Link QuickControl with Immersive mode to replace jb's fullscreen
    - Better menus
    - Data saver
    - CleanUp
    
    Answer this form to make browser better: http://goo.gl/forms/sekBy8sJc2
    
    Change-Id: I2fde4242f477359dc8af18d1b7752750058cd6e9
    Signed-off-by: linuxxxxx <joey@cyanogenmoditalia.it>

--- src/com/android/browser/preferences/WebsiteSettingsFragment.java
+++ src/com/android/browser/preferences/WebsiteSettingsFragment.java
@@ -219,9 +219,9 @@ public class WebsiteSettingsFragment extends ListFragment implements OnClickList
             mUsageHighIcon = BitmapFactory.decodeResource(getResources(),
                     R.drawable.ic_list_data_large);
             mLocationAllowedIcon = BitmapFactory.decodeResource(getResources(),
-                    R.drawable.ic_gps_on_dark);
+                    R.drawable.ic_gps_on);
             mLocationDisallowedIcon = BitmapFactory.decodeResource(getResources(),
-                    R.drawable.ic_gps_denied_dark);
+                    R.drawable.ic_gps_denied);
             mCurrentSite = site;
             if (mCurrentSite == null) {
                 askForOrigins();
