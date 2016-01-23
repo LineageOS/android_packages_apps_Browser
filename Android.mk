aa3189202df1c4fc7bd116c0340e1dd8f4391e01
jrizzoli <joey@cyanogenmoditalia.it>
2016-01-23 11:11:43 +0100

    Browser: be gello-friendly
    Change-Id: I7ff50329414835a0b58e1200c53b6f40964b9730
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+ifneq ($(WITH_GELLO_SOURCE),true)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -34,3 +35,4 @@ include $(BUILD_PACKAGE)
 
 # additionally, build tests in sub-folders in a separate .apk
 include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
